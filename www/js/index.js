1632f26f4f2edac9a00c878e1dd2006e8474615d
Alex Gladilov <a.gladilov@yandex.ru>
2018-09-13 23:23:37 +0300

    fix camera preview test

--- www/js/index.js
+++ www/js/index.js
@@ -58,9 +58,9 @@ var app = {
     },
 
     cameraPreviewTest: function() {
-        var appElement = document.getElementById('app');
-        appElement.setAttribute('style', 'display:none;');
-        document.body.classList.add("cameraPreview");
+        // var appElement = document.getElementById('app');
+        // appElement.setAttribute('style', 'display:none;');
+        // document.body.classList.add("cameraPreview");
 
         var options = {
             x: 0,
@@ -69,7 +69,7 @@ var app = {
             height: window.screen.height,
             camera: CameraPreview.CAMERA_DIRECTION.FRONT,
             toBack: true,
-            tapPhoto: true,
+            tapPhoto: false,
             tapFocus: true,
             previewDrag: false
         };
